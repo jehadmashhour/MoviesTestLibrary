93ba831e93f223e29d10ce7df559216098776b0a
jabdalqader <jabdalqader@Madfooat.com>
2020-06-28 00:51:05 +0300

    Second commit

--- movietestlibrary/src/main/java/com/madfooatcom/movietestlibrary/MovieInitializer.kt
+++ movietestlibrary/src/main/java/com/madfooatcom/movietestlibrary/MovieInitializer.kt
@@ -6,6 +6,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 object MovieInitializer {
+    @JvmStatic
     fun init(context: Context) {
         startKoin {
             androidContext(context)
